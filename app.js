588db79982921d3952f7d3527dfc494073880d2a
dinsky21 <90211190+dinsky21@users.noreply.github.com>
2022-10-08 22:46:57 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -13,16 +13,16 @@ function renderPeopleList(data) {
   let rawHTML = "";
   data.forEach((item) => {
     // full name, image
-    rawHTML += `<div class="col-sm-3">
-    <div class="mb-2">
-      <div class="card">
-        <img src="${item.avatar}" class="card-img-top" alt="Person Picture">
-        <div class="card-body">
-          <h5 class="card-title">${item.name} ${item.surname}</h5>
+    rawHTML += `<div class='col-8 col-lg-2'>
+    <div class='mb-2'>
+      <div class='card'>
+        <img src='${item.avatar}' class='img-fluid card-img-top' alt='Person Picture'>
+        <div class='card-body'>
+          <h6 class='card-title'>${item.name} ${item.surname}</h6>
         </div>
-        <div class="card-footer">
-          <button class="btn btn-primary btn-show-people" data-bs-toggle="modal" data-bs-target="#people-modal" data-id="${item.id}">More</button>
-          <button class="btn btn-info btn-add-favorite">+</button>
+        <div class='card-footer align-content-center'>
+          <button class='btn btn-primary btn-show-people' data-bs-toggle='modal' data-bs-target='#people-modal' data-id='${item.id}'>More</button>
+          <button class='btn btn-info btn-add-favorite'>+</button>
         </div>
       </div>
     </div>
@@ -39,6 +39,7 @@ axios
     people.push(...response.data.results);
     renderPage(people);
     renderPeopleList(showPeoplePerPage(1));
+    inputValue.value = "";
   })
   .catch((err) => console.log(err));
 
@@ -64,34 +65,59 @@ function showPeopleModal(id) {
     modalName.innerText = data.name + " " + data.surname;
     modalOrigin.innerText = "Come from : " + data.region;
     modalBday.innerText = "Birthday : " + data.birthday;
-    modalImage.innerHTML = `<img src="${data.avatar}" alt="people-poster" class="img-fluid">`;
+    modalImage.innerHTML = `<img src='${data.avatar}' alt='people-poster' class='img-fluid'>`;
   });
 }
 
-// 1. 將搜尋表單綁定提交事件，觸發搜尋功能
-// form 本身有submit這個事件，所以我們可用以監聽，並設定event handler為filterPeople
-formInput.addEventListener("submit", function filterPeople(event) {
+inputValue.addEventListener("keyup", function filterPeople(event) {
   event.preventDefault();
-  // 2. 取得搜尋框中使用者輸入的關鍵字
-  const keyword = inputValue.value.trim().toLowerCase(); //要取出輸入的值，需要用.value放進新變數
-  // 3. 比對搜尋關鍵字與電影標題
-  filteredList = people.filter(
-    (el) =>
-      el.name.toLowerCase().includes(keyword) ||
-      el.surname.toLowerCase().includes(keyword)
-  );
-  // 4. 將匹配結果回傳到網頁畫面上
-  if (filteredList.length) {
-    renderPeopleList(filteredList);
+
+  const keyword = inputValue.value.trim().toLowerCase();
+  if (keyword == "") {
     inputValue.value = "";
+    return;
+  } else {
+    filteredList = people.filter(
+      (el) =>
+        el.name.toLowerCase().includes(keyword) ||
+        el.surname.toLowerCase().includes(keyword)
+    );
   }
-  //4-1.若輸入空白鍵或是關鍵字無法匹配，keyword會直接被刪除，並且列出所有人
-  else {
+  if (filteredList.length) {
+    renderPage(filteredList);
+    renderPeopleList(showPeoplePerPage(1));
+  } else {
     renderPeopleList(people);
     alert("Cannot find matching people");
     inputValue.value = "";
   }
 });
+
+// // 1. 將搜尋表單綁定提交事件，觸發搜尋功能
+// // form 本身有submit這個事件，所以我們可用以監聽，並設定event handler為filterPeople
+// formInput.addEventListener('submit', function filterPeople(event) {
+//   event.preventDefault();
+//   // 2. 取得搜尋框中使用者輸入的關鍵字
+//   const keyword = inputValue.value.trim().toLowerCase(); //要取出輸入的值，需要用.value放進新變數
+//   // 3. 比對搜尋關鍵字與電影標題
+//   filteredList = people.filter(
+//     (el) =>
+//       el.name.toLowerCase().includes(keyword) ||
+//       el.surname.toLowerCase().includes(keyword)
+//   );
+//   // 4. 將匹配結果回傳到網頁畫面上
+//   if (filteredList.length) {
+//     renderPage(filteredList);
+//     renderPeopleList(showPeoplePerPage(1));
+//     inputValue.value = '';
+//   }
+//   //4-1.若輸入空白鍵或是關鍵字無法匹配，keyword會直接被刪除，並且列出所有人
+//   else {
+//     renderPeopleList(people);
+//     alert('Cannot find matching people');
+//     inputValue.value = '';
+//   }
+// });
 
 //建立加為好友清單的功能
 function addToFav(id) {
@@ -109,17 +135,64 @@ const paginator = document.querySelector("#paginator");
 const personPerPage = 24;
 
 function showPeoplePerPage(page) {
+  let data = filteredList.length ? filteredList : people; //
   const startIndex = (page - 1) * personPerPage;
-  return people.slice(startIndex, startIndex + personPerPage);
+  return data.slice(startIndex, startIndex + personPerPage);
 }
+// function showFilteredPeoplePerPage(page) {
+//   const startIndex = (page - 1) * personPerPage;
+//   return filteredList.slice(startIndex, startIndex + personPerPage);
+// }
 
 function renderPage(arr) {
   const numberOfPages = Math.ceil(arr.length / personPerPage);
   let rawHTML = ``;
   for (i = 1; i <= numberOfPages; i++) {
-    rawHTML += `<li class="page-item"><a class="page-link" href="#">${i}</a></li>`;
+    rawHTML += `<li class='page-item'><a class='page-link' href='#'>${i}</a></li>`;
   }
   paginator.innerHTML = rawHTML;
 }
 
-paginator.addEventListener("click", function showPage(event) {});
+paginator.addEventListener("click", function showPage(event) {
+  //如果被點擊的不是 a 標籤，結束
+  if (event.target.tagName !== "A") return;
+  let findPage = Number(event.target.innerText);
+  renderPeopleList(showPeoplePerPage(findPage));
+});
+
+//Sort 按名字和出生年份
+const sortName = document.querySelector("#sort-by-name");
+const sortYear = document.querySelector("#sort-by-birth-year");
+function sortByName(arr) {
+  arr.sort(function (a, b) {
+    let nameA = a.name.toLowerCase();
+    let nameB = b.name.toLowerCase();
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    } else 0;
+  });
+}
+
+sortName.addEventListener("click", function (event) {
+  event.preventDefault();
+  let data = filteredList.length ? filteredList : people;
+  sortByName(data);
+  renderPage(data);
+  renderPeopleList(showPeoplePerPage(1));
+});
+
+sortYear.addEventListener("click", function (event) {
+  event.preventDefault();
+  let data = filteredList.length ? filteredList : people;
+
+  data.sort(function (a, b) {
+    let dateA = new Date(a.birthday),
+      dateB = new Date(b.birthday);
+    return dateB - dateA;
+  });
+  renderPage(data);
+  renderPeopleList(showPeoplePerPage(1));
+});
--- favorite.js
+++ favorite.js
@@ -1,19 +1,20 @@
 const dataPanel = document.querySelector("#data-panel");
-const list = JSON.parse(localStorage.getItem("FavoriteList"));
+const list = JSON.parse(localStorage.getItem("FavoriteList")) || [];
 const rmvBtn = document.querySelector(".btn-rmv-favorite");
-
+const paginator = document.querySelector("#paginator");
+const personPerPage = 24;
 function renderPeopleList(data) {
   let rawHTML = "";
   data.forEach((item) => {
     // full name, image
-    rawHTML += `<div class="col-sm-3">
+    rawHTML += `<div class="col-8 col-lg-2">
       <div class="mb-2">
         <div class="card">
           <img src="${item.avatar}" class="card-img-top" alt="Person Picture">
           <div class="card-body">
-            <h5 class="card-title">${item.name} ${item.surname}</h5>
+            <h6 class="card-title">${item.name} ${item.surname}</h6>
           </div>
-          <div class="card-footer">
+          <div class="card-footer align-content-center">
             <button class="btn btn-primary btn-show-people" data-bs-toggle="modal" data-bs-target="#people-modal" data-id="${item.id}">More</button>
             <button class="btn btn-info btn-rmv-favorite">X</button>
           </div>
@@ -23,8 +24,13 @@ function renderPeopleList(data) {
   });
   dataPanel.innerHTML = rawHTML;
 }
-
-renderPeopleList(list);
+// console.log(list);
+if (list.length == 0) {
+  dataPanel.innerHTML = `<div class="col-sm-12"><h3 class="text-center" >Please add friends from home page</h3></div>`;
+} else {
+  renderPage(list);
+  renderPeopleList(showPeoplePerPage(1));
+}
 
 dataPanel.addEventListener("click", (event) => {
   if (event.target.matches(".btn-show-people")) {
@@ -32,7 +38,6 @@ dataPanel.addEventListener("click", (event) => {
   } else if (event.target.matches(".btn-rmv-favorite")) {
     let findId = event.target.previousElementSibling.dataset.id; //也可以在button的標籤中放入data-id，就不需要加previousElementSibling
     removeFromFavorite(Number(findId)); //dataset抓來的id，Number要加啊啊啊啊啊啊
-    // console.log(list);
   }
 });
 
@@ -42,3 +47,24 @@ function removeFromFavorite(id) {
   renderPeopleList(list);
   localStorage.setItem("FavoriteList", JSON.stringify(list));
 }
+
+//Paginator
+
+function showPeoplePerPage(page) {
+  const startIndex = (page - 1) * personPerPage;
+  return list.slice(startIndex, startIndex + personPerPage);
+}
+
+function renderPage(arr) {
+  const numberOfPages = Math.ceil(arr.length / personPerPage);
+  let rawHTML = ``;
+  for (i = 1; i <= numberOfPages; i++) {
+    rawHTML += `<li class="page-item"><a class="page-link" href="#">${i}</a></li>`;
+  }
+  paginator.innerHTML = rawHTML;
+}
+
+paginator.addEventListener("click", function showPage(event) {
+  let findPage = Number(event.target.innerText);
+  renderPeopleList(showPeoplePerPage(findPage));
+});
